3021b7ac45dbaf57f4e3cc596cf512a6f732399a
marialuiza2804 <167330826+marialuiza2804@users.noreply.github.com>
2025-06-30 11:16:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador do Brasil?',
+    'Resposta: Dom Pedro I'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Resposta: Rússia.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'Resposta: É uma palavra que indica ação, estado ou fenômeno da natureza (ex: correr, ser, chover).'
+    
+)
+criaCartao(
+    'Lingua inglesa',
+    'Traduza para o inglês: “Eu tenho um livro',
+    'Resposta: I have a book?',
+)
